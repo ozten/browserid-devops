49b3338c1246b9814610832fed8f97da87a051dc
Austin King <shout@ozten.com>
2012-08-06 11:22:39 -0700

    Adding BrowserID binary dev dependencies gmp-devel, openssl-devel

--- puppet/manifests/browserid.pp
+++ puppet/manifests/browserid.pp
@@ -1,9 +1,18 @@
+group { "browserid":
+  ensure => "present",
+}
+
+user { "browserid":
+  ensure => "present",
+  home => "/home/browserid"
+}
+
 group { "puppet":
   ensure => "present",
 }
 File { owner => 0, group => 0, mode => 0644 }
 
-file { '/etc/motd':
+file { "/etc/motd":
   content => "Welcome to your Vagrant-built virtual machine! Managed by Puppet.\n"
 }
 
@@ -14,6 +23,33 @@ service { "mysqld":
   enable => true
 }
 
-package {'git': ensure => 'present'}
+package { "git": ensure => "present" }
 
-package {'wget': ensure => 'present'}
+package { "wget": ensure => "present" }
+
+file {"/service/browserid/supervise":
+  ensure => "directory"
+}
+
+package {"gmp": ensure => "present" }
+
+package {"gmp-devel": ensure => "present" }
+
+package {"openssl": ensure => "present" }
+
+package {"openssl-devel": ensure => "present" }
+
+git clone git://github.com/mozilla/browserid.git
+
+file {"/service/browserid/run":
+  ensure => "present",
+  source => "puppet:///files/daemontools/browserid/run"
+}
+
+# This still has some errors
+# Had to pin jwcrypto to 0.3.1 manually before running
+exec { "browserid node dependencies installed":
+  command => "/usr/bin/npm install",
+  cwd => "/vagrant/browserid",
+  user => "browserid"
+}
