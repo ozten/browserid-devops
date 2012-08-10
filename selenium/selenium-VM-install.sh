88c9f0f81ea0716e8b53dc9ac573b2989dfa2e2e
Jared Hirsch <ohai@6a68.net>
2012-08-10 16:39:48 -0700

    Add tarball with browserid job, install it as part of VM install script

--- selenium/selenium-VM-install.sh
+++ selenium/selenium-VM-install.sh
@@ -13,6 +13,9 @@ wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key 
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -q -y jenkins
+
+# also install the browserid job
+tar xzvf /vagrant/selenium/jobs.tar.gz /var/lib/jenkins/
 echo "*** donezo ***" > 2
 
 # jenkins should now be running on localhost:8080 inside that VM.
